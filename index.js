499b97872dd483e865b8b476be60217723096208
Preet Mehta <mehtapreet404@gmail.com>
2021-07-29 11:06:45 +0530

    29-7-2021

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.ORIGIN,
+    origin: "https://chat-app-socketio.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
